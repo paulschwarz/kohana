713c4a9d0b4da8ceac779f195d867b23df028aed
Paul Schwarz <paulsschwarz@gmail.com>
2012-05-06 20:16:05 +0300

    Improved English in comment.

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ if ( ! defined('KOHANA_START_MEMORY'))
 // Bootstrap the application
 require APPPATH.'bootstrap'.EXT;
 
-if (PHP_SAPI == 'cli') // Try and load minion
+if (PHP_SAPI == 'cli') // Try to load minion
 {
 	class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
 	set_exception_handler(array('Minion_Exception', 'handler'));
